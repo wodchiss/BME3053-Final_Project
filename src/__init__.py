75f9eda2c68da2c808333fc6cbafd6514bdc6625
Sophya Wodchis <127273924+wodchiss@users.noreply.github.com>
2025-04-26 10:55:40 +0000

    adjusted __init__.py

---
+++ src/__init__.py
@@ -0,0 +1,13 @@
+# filepath: /workspaces/BME3053-Final_Project/src/main.py
+from preprocess import preprocess_data
+from train import train_model
+from evaluate_model import evaluate_model
+
+# Preprocess data
+preprocess_data()
+
+# Train model
+train_model()
+
+# Evaluate model
+evaluate_model()
